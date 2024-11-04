f57dee5846ed596024f2324afe341588cbbf048b
Luis Rojas <luisrojasgarzon@gmail.com>
2024-11-04 16:13:06 -0300

    version 1.0

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id:0,
+        title: 'minha primeira tarefa',
+        description:"minha primeira tarefa",
+    },
+    {
+        id:1,
+        title: 'minha segunda tarefa',
+        description:"minha segunda tarefa",
+    },
+    {
+        id:2,
+        title: 'minha terceira tarefa',
+        description:"minha terceira tarefa",
+    }
+]
